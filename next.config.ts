606cfd6ef52a569d94a93906094e20c502ca5365
amanu-taye <111277320+amanu-taye@users.noreply.github.com>
2026-03-05 09:38:18 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  images: {
+    qualities: [100, 75],
+  },
+};
+
+export default nextConfig;
